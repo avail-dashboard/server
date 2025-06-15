aaeb953d5735742210f2e09f3c5d5b048de6ce13
Vineet Kumar <vinetchoudhary@gmail.com>
2025-06-15 10:59:57 +0530

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,8 @@ module.exports = {
       script: 'npm start',
       env_production: {
         NODE_ENV: 'production',
-        PORT: 3001
+        PORT: 3001,
+        ENV_FILE: '.env.production'
       },
       pre_deploy: 'npm run build'
     }
